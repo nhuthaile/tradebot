c17b9f26940a404b929c8332f267061e58e3ce2b
HAI LE <nhuthai.le@gmail.com>
2024-05-10 22:42:07 +0700

    file chart, ghi thong tin ra file txt

---
+++ chart.js
@@ -0,0 +1,28 @@
+const chart = require("asciichart");
+const fs = require("fs");
+
+function drawChartV1() {
+  const lines = fs.readFileSync("./trade_log_1.txt", "utf8").split("\n");
+  const assets = [];
+
+  for (const line of lines) {
+    if (line.includes("P/L USDT:")) {
+      const asset = line.replace("P/L USDT:  ", "").trim();
+      assets.push(parseFloat(asset));
+    }
+  }
+  console.clear();
+  console.log(
+    chart.plot(assets, {
+      height: 30,
+    })
+  );
+}
+
+drawChartV1();
+
+fs.watch("./trade_log.txt", (event, filename) => {
+  if (event === "change") {
+    drawChartV1();
+  }
+});
